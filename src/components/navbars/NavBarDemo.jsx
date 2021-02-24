a278822a562a06a11073da1b8978a3d6bc2ac10b
bwkarr77 <bwkarr77@gmail.com>
2021-02-23 21:57:11 -0600

    navbar details worked on

--- src/components/navbars/NavBarDemo.jsx
+++ src/components/navbars/NavBarDemo.jsx
@@ -1,18 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { AccountCircle, Search, Clear } from "@material-ui/icons";
+import { Button } from "@material-ui/core";
+
 import "./navbar.scss";
 
 const NavBarDemo = ({ isLoggedIn }) => {
   return (
     <nav className="navbar">
-      <div className="logo">Logo</div>
-      <div className="search">Search</div>
-      <div className="nav-access">
-        <div className="login-button">Login</div>
-        <div className="signup-button">Sign-Up</div>
+      <div className="logo">
+        <a
+          className="fa fa-reddit"
+          target="_blank"
+          rel="noopener noreferrer"
+          href=""
+          aria-hidden="true"
+        ></a>
+        Logo
       </div>
-      <div className="user-settings">Settings</div>
+      <div className="search-bar">
+        <Search />
+        <input className="search-input" value="Search" />
+        <Clear />
+      </div>
+      <div className="nav-access">
+        <div className="login-button">
+          <Button variant="contained" border-radius="50%" className="logIn">
+            Log In
+          </Button>
+        </div>
+        <div className="signup-button">
+          <Button variant="contained" color="primary" className="signIn">
+            Sign Up
+          </Button>
+        </div>
+        <div className="user-settings">
+          <AccountCircle />
+          Settings
+        </div>
+      </div>
     </nav>
   );
 };
