aadef6ba82921e5184fd8ee989b2d9e7dad548a0
bwkarr77 <bwkarr77@gmail.com>
2021-02-24 21:28:46 -0600

    navbar components created, still need styling

--- src/components/navbars/NavBarDemo.jsx
+++ src/components/navbars/NavBarDemo.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { AccountCircle, Search, Clear } from "@material-ui/icons";
+import {
+  AccountCircle,
+  Search,
+  Clear,
+  Chat,
+  Mail,
+  Create,
+  Videocam,
+  Equalizer,
+  TrendingUp,
+} from "@material-ui/icons";
 import { Button } from "@material-ui/core";
 
 import "./navbar.scss";
@@ -16,15 +26,28 @@ const NavBarDemo = ({ isLoggedIn }) => {
           rel="noopener noreferrer"
           href=""
           aria-hidden="true"
-        ></a>
-        Logo
+        >
+          <span>Logo</span>
+        </a>
       </div>
+      <div className="category"></div>
       <div className="search-bar">
-        <Search />
+        <Search className="search-icon" />
         <input className="search-input" value="Search" />
-        <Clear />
+        <Clear className="search-clear" />
       </div>
       <div className="nav-access">
+        <div className="left-section">
+          {/*??, equalizer,  */}
+          <TrendingUp className="left-buttons" />
+          <Equalizer className="left-buttons" />
+          <Videocam className="left-buttons" />
+        </div>
+        <div className="right-section">
+          <Chat className="right-buttons" />
+          <Mail className="right-buttons" />
+          <Create className="right-buttons" />
+        </div>
         <div className="login-button">
           <Button variant="contained" border-radius="50%" className="logIn">
             Log In
